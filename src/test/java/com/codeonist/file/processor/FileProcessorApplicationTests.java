1f5d106684dbfb9c7ea7c8c2f46c09a719c6004f
Sourabh Sarkar <sourabhsarkar7@gmail.com>
2021-09-23 22:22:05 +0530

    First Commit

---
+++ src/test/java/com/codeonist/file/processor/FileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeonist.file.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
